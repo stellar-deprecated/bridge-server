72f73cf3f39f1b2e1a9ff86681a8c42cabf81546
Peter Oliha <poliha2002@yahoo.com>
2017-08-01 01:01:03 +0100

    fetch seqNum in /builder

--- src/github.com/stellar/gateway/bridge/handlers/request_handler_builder.go
+++ src/github.com/stellar/gateway/bridge/handlers/request_handler_builder.go
@@ -42,10 +42,20 @@ func (rh *RequestHandler) Builder(w http.ResponseWriter, r *http.Request) {
 
 	sequenceNumber, err := strconv.ParseUint(request.SequenceNumber, 10, 64)
 	if err != nil {
-		errorResponse := protocols.NewInvalidParameterError("sequence_number", request.SequenceNumber, "Sequence number must be a number")
-		log.WithFields(errorResponse.LogData).Error(errorResponse.Error())
-		server.Write(w, errorResponse)
-		return
+		accountResponse, err := rh.Horizon.LoadAccount(request.Source)
+		if err != nil {
+			errorResponse := protocols.NewInvalidParameterError("sequence_number", request.SequenceNumber, "Sequence number must be a number. Attempt to fetch failed.")
+			log.WithFields(errorResponse.LogData).Error(errorResponse.Error())
+			server.Write(w, errorResponse)
+			return
+		}
+		sequenceNumber, err = strconv.ParseUint(accountResponse.SequenceNumber, 10, 64)
+		if err != nil {
+			errorResponse := protocols.NewInvalidParameterError("sequence_number", request.SequenceNumber, "Sequence number must be a number. Attempt to fetch failed.")
+			log.WithFields(errorResponse.LogData).Error(errorResponse.Error())
+			server.Write(w, errorResponse)
+			return
+		}
 	}
 
 	mutators := []b.TransactionMutator{
